8edd012393d4513b645f52f66e36b062517880e8
yuki hiragino <hiraginoyuki@gmail.com>
2020-12-14 01:00:57 +0900

    Make indent more easy-to-read

--- src/fifteen-puzzle/renderer.tsx
+++ src/fifteen-puzzle/renderer.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { FifteenPuzzle } from './fifteen-puzzle';
 import styles from './renderer.module.scss';
+import { useForceUpdate } from '../utils';
 
 export interface FifteenPuzzleRendererProps {
   puzzle: FifteenPuzzle;
 }
 export function FifteenPuzzleRenderer(props: FifteenPuzzleRendererProps) {
-  return <div className={styles.fifteenPuzzleRenderer} data-columns={props.puzzle.columns} data-rows={props.puzzle.rows}>
-    
-  </div>;
+  const forceUpdate = useForceUpdate();
+
+  const numbers = props.puzzle.getNumbers();
+  return (
+    <div className={styles.fifteenPuzzleRenderer}
+         data-columns={props.puzzle.columns}
+         data-rows={props.puzzle.rows}>
+      { numbers.map((number, i) => {
+        const point = props.puzzle.getPoint(i);
+        return <div key={i}
+                    data-x={point[0]}
+                    data-y={point[1]}
+                    onClick={() => (props.puzzle.tap(point), forceUpdate())}> {number} </div>;
+      }) }
+    </div>
+  );
 }
