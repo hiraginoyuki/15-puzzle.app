c6329020fbcb28ebbe7be5b4357c256311863fec
yuki hiragino <hiraginoyuki@gmail.com>
2020-12-17 00:04:21 +0900

    Add renderer

--- src/fifteen-puzzle/renderer.tsx
+++ src/fifteen-puzzle/renderer.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { FifteenPuzzle } from './fifteen-puzzle';
+import { useForceUpdate, joinClassNames, range } from '../utils';
 import styles from './renderer.module.scss';
-import { useForceUpdate } from '../utils';
 
 export interface FifteenPuzzleRendererProps {
   puzzle: FifteenPuzzle;
 }
 export function FifteenPuzzleRenderer(props: FifteenPuzzleRendererProps) {
   const forceUpdate = useForceUpdate();
-
   const numbers = props.puzzle.getNumbers();
   return (
     <div className={styles.fifteenPuzzleRenderer}
-         data-columns={props.puzzle.columns}
-         data-rows={props.puzzle.rows}>
-      { numbers.map((number, i) => {
-        const point = props.puzzle.getPoint(i);
-        return <div key={i}
-                    data-x={point[0]}
-                    data-y={point[1]}
-                    onClick={() => (props.puzzle.tap(point), forceUpdate())}> {number} </div>;
+         style={{ "--columns": props.puzzle.columns, "--rows": props.puzzle.rows } as CSSProperties}>
+      <style>
+      {`
+        .flex-container {
+          width: 100%;
+          height: 100%;
+          justify-content: center;
+        }
+        .flex-container > div {
+          margin: auto auto;
+        }
+      `}
+      </style>
+      { range(numbers.length).map((number) => {
+        const point = props.puzzle.getPoint(number);
+        const index = props.puzzle.toIndex(point);
+        const onTap = () => (props.puzzle.tap(point), console.log("tapped", point), forceUpdate());
+        return <div className={joinClassNames(styles.piece, number === index + 1 && styles.isWhereItShouldBe, number === 0 && styles.zero)}
+                    style={{ "--x": point[0], "--y": point[1] } as CSSProperties}
+                    key={number}
+                    onTouchStart={onTap}
+                    onMouseDown={onTap}>
+          <div className={styles.number}> {number} </div>
+        </div>;
       }) }
     </div>
   );
