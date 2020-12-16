664d7ee9e03516b2f4248ae076d2699c383ce9f4
yuki hiragino <hiraginoyuki@gmail.com>
2020-12-17 00:05:44 +0900

    Make app render puzzle

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { FifteenPuzzle } from "./fifteen-puzzle";
+import { FlexCenteringContainer } from "./utils";
+
+const puzzle = FifteenPuzzle.generateRandom(4);
+Object.defineProperties(window, {
+  FifteenPuzzle: { value: FifteenPuzzle },
+  puzzle: { value: puzzle }
+});
 
 export function App() {
   return <>
+    <FlexCenteringContainer>
+      <FifteenPuzzle.Renderer puzzle={puzzle} />
+    </FlexCenteringContainer>
   </>;
 }
