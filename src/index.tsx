3ba38c753a926036d662bed2d3b142f53fed4c83
yuki hiragino <hiraginoyuki@gmail.com>
2020-12-19 21:47:03 +0900

    Change how service-worker's methods are exported

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./app";
 import './index.scss';
-import * as serviceWorker from "./utils";
+import { serviceWorker } from "./utils";
 
 ReactDOM.render(<App />, document.getElementById("app"));
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,5 +7,5 @@ export * from './log';
 export * from './range';
 export * as Mapper from "./mapper";
 export * from './mapper';
-export * from './service-worker';
+export * as serviceWorker from './service-worker';
 export * from './use-force-update';
