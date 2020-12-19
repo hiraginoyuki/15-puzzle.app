22247bd249b133c571da2ab057304e7b89c2041d
tenzyu <tenzyu.on@gmail.com>
2020-12-19 18:40:33 +0900

    Preparing for PWA

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./app";
 import './index.scss';
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("app"));
+
+serviceWorker.register();
