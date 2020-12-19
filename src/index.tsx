619462675bfd5cdb41a7d584760307ad029e4237
yuki hiragino <hiraginoyuki@gmail.com>
2020-12-19 21:43:33 +0900

    Move and rename service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./app";
 import './index.scss';
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./utils";
 
 ReactDOM.render(<App />, document.getElementById("app"));
 
