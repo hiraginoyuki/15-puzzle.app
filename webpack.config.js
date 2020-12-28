9f2a4f190b846fbe59a2a8ac0cd5f9b53a8605c1
yuki hiragino <hiraginoyuki@gmail.com>
2020-12-29 02:06:18 +0900

    Install webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
-const { resolve } = require("path");
+const path = require("path");
+const { resolve } = path;
+const isDevelopment = process.env.NODE_ENV === "development";
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
-  mode: process.env.NODE_ENV || "production",
-  devtool: 'source-map',
+  mode: isDevelopment ? "development" : "production",
+  cache: true,
 
   entry: './src/index.tsx',
-  resolve: { extensions: [ '.ts', '.tsx', '.js', '.scss' ] },
-  output: { path: resolve(__dirname, 'public/build'), filename: './bundle.js' },
+  resolve: { extensions: [ '.ts', '.tsx', '.js', '.css', '.scss' ] },
+  output: {
+    path: resolve(__dirname, 'public/build'),
+    filename: './bundle.js',
+    publicPath: resolve(__dirname, './public/'),
+  },
+  devServer: {
+    writeToDisk: (filePath) => !/.*\.hot-update\..+$/.test(filePath),
+    inline: true,
+    hot: false,
+    port: 8080,
+    publicPath: resolve(__dirname, './public/'),
+    contentBase: [ './src/', './public/', './public/build/' ],
+  },
 
   module: {
     rules: [
@@ -19,10 +34,20 @@ module.exports = {
               modules: { mode: 'local', localIdentName: "[local]__[hash:base64:8]" }
             }
           },
-          { loader: 'sass-loader' }
+          { loader: 'sass-loader' },
         ]
       },
-      { test: /\.tsx?$/, loader: 'ts-loader' }
+      { test: /\.tsx?$/,
+        use: [
+          { loader: require.resolve("babel-loader"),
+            options: {
+              plugins: [ isDevelopment && require.resolve("react-refresh/babel") ].filter(Boolean)
+            }
+          },
+          { loader: 'ts-loader' },
+        ]
+      }
     ]
-  }
+  },
+  plugins: [ isDevelopment && new ReactRefreshWebpackPlugin() ].filter(Boolean)
 };
