c8dfbb032f323704e52d0dc0d97b88e1be971c97
yuki hiragino <hiraginoyuki@gmail.com>
2021-01-04 12:48:03 +0900

    Change indent

--- src/fifteen-puzzle/renderer.tsx
+++ src/fifteen-puzzle/renderer.tsx
@@ -13,7 +13,8 @@ export function FifteenPuzzleRenderer(props: FifteenPuzzleRendererProps) {
   const numbers = props.puzzle.getNumbers();
   return (
     <div className={styles.fifteenPuzzleRenderer}
-         style={{ "--columns": props.puzzle.columns, "--rows": props.puzzle.rows } as CSSProperties}>
+         style={{ "--columns": props.puzzle.columns, "--rows": props.puzzle.rows } as CSSProperties}
+    >
       { range(numbers.length).map((number) => {
         const point = props.puzzle.getPointFromValue(number);
         const index = props.puzzle.pointUtil.convertPointToIndex(point);
@@ -21,7 +22,8 @@ export function FifteenPuzzleRenderer(props: FifteenPuzzleRendererProps) {
         return <div className={joinClassNames(styles.piece, number === index + 1 && styles.correct, number === 0 && styles.zero)}
                     style={{ "--x": point[0], "--y": point[1] } as CSSProperties}
                     key={number}
-                    {...{ [EVENT]: onTap }}>
+                    {...{ [EVENT]: onTap }}
+        >
           <div className={styles.number}> {number} </div>
         </div>;
       }) }
