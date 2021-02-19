6ceb8285dfd40949283904b2250bd75ded73e1d4
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-02-19 01:16:03 -0800

    Split to Piece and use style.var

--- src/app/renderer/renderer.tsx
+++ src/app/renderer/renderer.tsx
@@ -1,6 +1,6 @@
-import React, { CSSProperties } from 'react';
-import { FifteenPuzzle } from '../../fifteen-puzzle';
-import { useForceUpdate, joinClassNames, range, isMobile } from '../../utils';
+import React, { PropsWithChildren } from 'react';
+import { FifteenPuzzle, Point2D } from '../../fifteen-puzzle';
+import { useForceUpdate, joinClassNames, range, isMobile, style } from '../../utils';
 import styles from './renderer.scss';
 
 export interface FifteenPuzzleRendererProps {
@@ -12,20 +12,34 @@ export function FifteenPuzzleRenderer(props: FifteenPuzzleRendererProps) {
   const numbers = props.puzzle.numbers;
   return (
     <div className={styles.fifteenPuzzleRenderer}
-         style={{ "--columns": props.puzzle.columns, "--rows": props.puzzle.rows } as CSSProperties}
+         style={style.var({ columns: props.puzzle.columns, rows: props.puzzle.rows })}
     >
       { range(numbers.length).map((number) => {
         const point = props.puzzle.getPointFromValue(number);
         const index = props.puzzle.pointUtil.convertPointToIndex(point);
         const onTap = () => (props.puzzle.tap(point), console.log("tapped", point), forceUpdate());
-        return <div className={joinClassNames(styles.piece, number === index + 1 && styles.correct, number === 0 && styles.zero)}
-                    style={{ "--x": point[0], "--y": point[1] } as CSSProperties}
-                    key={number}
-                    {...{ [TAP_EVENT]: onTap }}
-        >
+        return <Piece key={number} zero={number == 0} correct={number == index + 1}
+                      tapEvent={TAP_EVENT} onTap={onTap}
+                      x={point[0]} y={point[1]}>
           <div className={styles.number}> {number} </div>
-        </div>;
+        </Piece>;
       }) }
     </div>
   );
+}
+
+interface PieceProps<Z extends boolean> {
+  x: number;
+  y: number;
+  tapEvent: "onTouchStart" | "onMouseDown";
+  onTap(point: Point2D): any;
+  key: string | number;
+  zero: Z;
+  correct: Z extends true ? false : boolean;
+}
+function Piece<Z extends boolean>(props: PropsWithChildren<PieceProps<Z>>) {
+  return <div className={joinClassNames(styles.piece, props.correct && styles.correct, props.zero && styles.zero)}
+              style={style.var({ x: props.x, y: props.y })}
+              key={props.key}
+              {...{ [props.tapEvent]: props.onTap }}> { props.children } </div>;
 }
