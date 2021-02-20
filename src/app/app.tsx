a85092788f78abc64be1b49d9d7cbe22433685d8
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-02-20 14:48:56 -0800

    IMPLEMENT RESET BUTTON FINALLY

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { FifteenPuzzle } from "../fifteen-puzzle";
 import { FifteenPuzzleRenderer } from "./renderer";
 import { FlexCenteringContainer, defineOnGlobal } from "../utils";
@@ -6,12 +6,9 @@ import styles from './app.scss';
 
 defineOnGlobal({ FifteenPuzzle });
 export function App() {
-  const [ puzzle, setPuzzle ] = useState(FifteenPuzzle.generateRandom(4));
-  useEffect(() => defineOnGlobal({ puzzle, setPuzzle }), [puzzle, setPuzzle]);
-
   return <>
     <FlexCenteringContainer>
-      <FifteenPuzzleRenderer puzzle={puzzle} />
+      <FifteenPuzzleRenderer />
       {/* <div style={{backgroundColor:"#fff",width:"100px",height:"100px"}} onClick={() => setPuzzle(FifteenPuzzle.generateRandom(4))}>reset</div> */}
     </FlexCenteringContainer>
     <div className={styles.name}>Made by Yuki Hiragino</div>
--- src/app/renderer/renderer.tsx
+++ src/app/renderer/renderer.tsx
@@ -1,14 +1,54 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useEffect, useRef, useState } from 'react';
 import { FifteenPuzzle, Point2D } from '../../fifteen-puzzle';
-import { useForceUpdate, joinClassNames, range, isMobile, style } from '../../utils';
+import { useForceUpdate, joinClassNames, range, isMobile, style, useComputedState, defineOnGlobal } from '../../utils';
 import styles from './renderer.scss';
 
-export interface FifteenPuzzleRendererProps {
-  puzzle: FifteenPuzzle;
+const keys = [
+  "4", "5", "6", "7",
+  "r", "t", "y", "u",
+  "f", "g", "h", "j",
+  "v", "b", "n", "m",
+] as const;
+const keyMap: { [key: string]: Point2D } = Object.fromEntries(
+  (Object.entries(keys) as any as [number, string][]).map(([index, key]) => [key, [index % 4, Math.floor(index / 4)]])
+);
+console.log(keyMap);
+
+function forceSolve(puzzle: FifteenPuzzle): FifteenPuzzle {
+  puzzle.numbers = [...Array(puzzle.columns * puzzle.rows - 1)].map((_, i) => i + 1).concat(0);
+  return puzzle;
 }
-export function FifteenPuzzleRenderer({ puzzle }: FifteenPuzzleRendererProps) {
+defineOnGlobal({ forceSolve });
+
+export function FifteenPuzzleRenderer() {
+  const [ puzzle, setPuzzle ] = useState(FifteenPuzzle.generateRandom(4));
   const TAP_EVENT = isMobile ? "onTouchStart" : "onMouseDown";
   const forceUpdate = useForceUpdate();
+  const listener = useRef(({ key }: KeyboardEvent) => onKeyDown(key));
+
+  function reset() {
+    setPuzzle(FifteenPuzzle.generateRandom());
+  }
+
+  function onTap(point: Point2D) {
+    puzzle.tap(point);
+    console.log("tapped", point);
+    forceUpdate();
+  };
+
+  function onKeyDown(key: string) {
+    if (key == " ") reset();
+    const point = keyMap[key];
+    console.log(key, point);
+    if (Array.isArray(point)) onTap(point);
+  }
+
+  useEffect(() => defineOnGlobal({ puzzle, setPuzzle, forceUpdate }));
+  useEffect(() => {
+    document.removeEventListener("keydown", listener.current);
+    document.addEventListener("keydown", listener.current = ({ key }) => onKeyDown(key));
+  }, [puzzle]);
+
   return (
     <div className={styles.fifteenPuzzleRenderer}
          style={style.var({ columns: puzzle.columns, rows: puzzle.rows })}
@@ -16,29 +56,33 @@ export function FifteenPuzzleRenderer({ puzzle }: FifteenPuzzleRendererProps) {
       { range(puzzle.numbers.length).map((number) => {
         const point = puzzle.getPointFromValue(number);
         const index = puzzle.pointUtil.convertPointToIndex(point);
-        const onTap = () => (puzzle.tap(point), console.log("tapped", point), forceUpdate());
-        return <Piece key={number} zero={number == 0} correct={number == index + 1}
-                      tapEvent={TAP_EVENT} onTap={onTap}
+        const isZero = number == 0;
+        const isSolved = useComputedState(() => puzzle.isSolved());
+        return <Piece key={number} hidden={isZero && !isSolved.value} correct={number == index + 1}
+                      tapEvent={TAP_EVENT} onTap={isZero && isSolved.value ? reset : onTap}
                       x={point[0]} y={point[1]}>
-          <div className={styles.number}> {number} </div>
+          { isZero
+            ? <div className={styles.number}> R </div>
+            : <div className={styles.number}> {number} </div>
+          }
         </Piece>;
       }) }
     </div>
   );
 }
 
-interface PieceProps<Z extends boolean> {
+interface PieceProps {
   x: number;
   y: number;
+  hidden: boolean;
   tapEvent: "onTouchStart" | "onMouseDown";
   onTap(point: Point2D): any;
   key: string | number;
-  zero: Z;
-  correct: Z extends true ? false : boolean;
+  correct: boolean;
 }
-function Piece<Z extends boolean>({ x, y, tapEvent, onTap, key, zero, correct, children }: PropsWithChildren<PieceProps<Z>>) {
-  return <div className={joinClassNames(styles.piece, correct && styles.correct, zero && styles.zero)}
+function Piece({ x, y, hidden, tapEvent, onTap, key, correct, children }: PropsWithChildren<PieceProps>) {
+  return <div className={joinClassNames(styles.piece, correct && styles.correct, hidden && styles.hidden)}
               style={style.var({ x, y })}
               key={key}
-              {...{ [tapEvent]: onTap }}> { children } </div>;
+              {...{ [tapEvent]: () => onTap([x, y]) }}> { children } </div>;
 }
