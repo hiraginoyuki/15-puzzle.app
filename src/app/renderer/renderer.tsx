8ff84b85ef280eecffad21ec683e17785fefaf5e
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-02-25 09:31:48 -0800

    Create and use PuzzleManager

--- src/app/renderer/renderer.tsx
+++ src/app/renderer/renderer.tsx
@@ -1,7 +1,7 @@
-import React, { PropsWithChildren, useEffect, useRef, useState } from 'react';
-import { FifteenPuzzle } from '15-puzzle';
-import { useForceUpdate, joinClassNames as join, range, isMobile, style, useComputedState, defineOnGlobal } from '../../utils';
+import React, { PropsWithChildren, useEffect, useRef } from 'react';
+import { useForceUpdate, joinClassNames as join, isMobile, style, defineOnGlobal } from '../../utils';
 import styles from './renderer.scss';
+import { PuzzleManager } from '../../puzzle-manager';
 
 type Point2D = [number, number];
 
@@ -12,24 +12,22 @@ const keyMap = {
   v:[0,3], b:[1,3], n:[2,3], m:[3,3],
 } as { [key: string]: Point2D };
 
-function forceSolve(puzzle: FifteenPuzzle): FifteenPuzzle {
-  puzzle.numbers = [...Array(puzzle.columns * puzzle.rows - 1)].map((_, i) => i + 1).concat(0);
-  return puzzle;
-}
-defineOnGlobal({ forceSolve });
+const TAP_EVENT = isMobile ? "onTouchStart" : "onMouseDown";
 
 export function FifteenPuzzleRenderer() {
-  const [puzzle, setPuzzle] = useState(FifteenPuzzle.generateRandom(4));
-  const TAP_EVENT = isMobile ? "onTouchStart" : "onMouseDown";
+  const { current: puzzleManager } = useRef(new PuzzleManager().generate());
+  const { isSolved } = puzzleManager;
   const forceUpdate = useForceUpdate();
   const listener = useRef<(event: KeyboardEvent) => any>();
 
+  defineOnGlobal({ puzzleManager, forceUpdate });
+
   function reset() {
-    setPuzzle(FifteenPuzzle.generateRandom());
+    puzzleManager.generate();
   }
 
   function onTap(point: Point2D) {
-    puzzle.tap(point);
+    puzzleManager.tap(point);
     console.log("tapped", point);
     forceUpdate();
   };
@@ -44,26 +42,23 @@ export function FifteenPuzzleRenderer() {
   useEffect(() => {
     document.removeEventListener("keydown", listener.current!);
     document.addEventListener("keydown", listener.current = ({ key }) => onKeyDown(key));
-  }, [puzzle]);
+  });
 
-  defineOnGlobal({ puzzle, setPuzzle, forceUpdate });
+  const { columns, rows } = puzzleManager.puzzleInstance;
   
   return (
     <div className={styles.fifteenPuzzleRenderer}
-         style={style.var({ columns: puzzle.columns, rows: puzzle.rows })}
+         style={style.var({ columns, rows })}
     >
       {
-        range(puzzle.numbers.length).map((number) => {
-          const coord = puzzle.getPointFromValue(number);
-          const index = puzzle.pointUtil.convertPointToIndex(coord);
+        puzzleManager.getNumbers().map(({ coord, number, isCorrect }) => {
           const isZero = number == 0;
-          const isSolved = useComputedState(() => puzzle.isSolved());
           const content = isZero
                         ? <div className={styles.number}> R </div>
                         : <div className={styles.number}> {number} </div>;
           return (
-            <Piece hidden={isZero && !isSolved.value} correct={number == index + 1}
-                   tapEvent={TAP_EVENT} onTap={isZero && isSolved.value ? reset : onTap}
+            <Piece hidden={isZero && !isSolved} correct={isCorrect}
+                   tapEvent={TAP_EVENT} onTap={isZero && isSolved ? reset : onTap}
                    coord={coord} key={number}>
               { content }
             </Piece>
---
+++ src/puzzle-manager.ts
@@ -0,0 +1,96 @@
+import { FifteenPuzzle } from "15-puzzle";
+import { range } from "./utils";
+
+type Point2D = [number, number];
+
+interface Game {
+  timeGenerated: number;
+  timeStarted: number;
+  timeEnded: number;
+  seed: string;
+  taps: Point2D[];
+}
+
+function getTimestamp() {
+  return +new Date;
+}
+  
+function getNewGame() {
+  return { taps: [] } as any as Game;
+}
+
+export class PuzzleManager {
+  public gameHistory: Game[];
+  public gameInfo: Game;
+  public puzzleInstance: FifteenPuzzle;
+
+  public constructor(private onUpdate: () => any = () => {}) {
+    this.gameHistory = [];
+    this.clean();
+  }
+
+  public setOnUpdate(onUpdate: () => any): this {
+    this.onUpdate = onUpdate;
+    return this;
+  }
+  
+  public started = false;
+  public isSolved = false;
+  private clean() {
+    if (this.puzzleInstance.isSolved()) {
+      this.gameHistory.push(this.gameInfo);
+    }
+    this.started = false;
+    this.isSolved = false;
+    this.gameInfo = getNewGame();
+  }
+
+  private updateSolvedState() {
+    this.isSolved = this.puzzleInstance.isSolved();
+  }
+
+  public generate(): this {
+    this.clean();
+    const timestamp = getTimestamp();
+    const seed = `${timestamp}`;
+    this.puzzleInstance = FifteenPuzzle.generateRandom(seed);
+    this.gameInfo.timeGenerated = timestamp;
+    this.gameInfo.seed = seed;
+    this.updateSolvedState();
+    return this;
+  }
+
+  public tap(coord: Point2D): boolean {
+    if (this.isSolved) return false;
+    const succeeded = this.puzzleInstance.tap(coord);
+    if (!succeeded) return false;
+    if (!this.started) {
+      this.started = true;
+      this.gameInfo.timeStarted = getTimestamp();
+    }
+    this.gameInfo.taps.push(coord);
+    this.updateSolvedState();
+    if (this.isSolved) this.onSolve();
+    this.onUpdate();
+    return true;
+  }
+
+  private onSolve() {
+    this.gameInfo.timeEnded = getTimestamp();
+  }
+
+  public getNumbers() {
+    const puzzle = this.puzzleInstance;
+    return range(1, puzzle.columns * puzzle.rows)
+      .map(number => {
+        const coord = puzzle.getPointFromValue(number);
+        const index = puzzle.pointUtil.convertPointToIndex(coord);
+        return { coord, number, isCorrect: number == index + 1 };
+      });
+  }
+
+  public forceSolve() {
+    const puzzle = this.puzzleInstance;
+    puzzle.numbers = [...Array(puzzle.columns * puzzle.rows - 1)].map((_, i) => i + 1).concat(0);
+  }
+}
