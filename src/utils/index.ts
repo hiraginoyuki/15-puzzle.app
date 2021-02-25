75d9330a1f308704f0408ed7fc207865f989bcd7
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-02-25 11:17:12 -0800

    Temporarily comment out unused modules

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,13 @@
 export * from './flex-centering-container';
-export * from './array';
-export * from './computed-state';
+// export * from './array';
+// export * from './computed-state';
 export * from './define-on-global';
 export * from './disable-scroll';
 export * from './is-mobile';
 export * from './join-class-names';
-export * from './log';
+// export * from './log';
 export * from './range';
-export * as Mapper from "./mapper";
-export * from './mapper';
+// export * as Mapper from "./mapper";
+// export * from './mapper';
 export * from './style';
 export * from './use-force-update';
