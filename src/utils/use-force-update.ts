ec51122eb0eff24185d3b9ea059ae3a6dfa1f1f3
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-03-03 07:01:35 -0800

    Optimize forceUpdate

--- src/utils/use-force-update.ts
+++ src/utils/use-force-update.ts
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import { useConstant } from './use-constant';
 
 export function useForceUpdate() {
   const [, setValue] = useState(0);
-  return () => setValue(value => value + 1);
+  const forceUpdate = useConstant(() => () => setValue(value => value + 1));
+  return forceUpdate;
 }
