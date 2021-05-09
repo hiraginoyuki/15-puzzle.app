14d241fde3863b70f8401fafef2e7b02ab538dc6
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-05-09 20:20:24 +0900

    Make use-animation-frame not clear every time

--- src/utils/use-animation-frame.ts
+++ src/utils/use-animation-frame.ts
@@ -1,16 +1,18 @@
 import React, { useEffect, useRef } from "react";
+import { useCallbackRef } from "./use-callback-ref";
 
 export function useAnimationFrame(onFrame: (time: number) => any, deps?: React.DependencyList) {
   const idRef = useRef(0);
+  const callbackRef = useCallbackRef(onFrame, deps || []);
 
   useEffect(() => {
     function frame(time: number) {
-      onFrame(time);
+      callbackRef.current(time);
       idRef.current = requestAnimationFrame(frame);
     }
     idRef.current = requestAnimationFrame(frame);
     return () => cancelAnimationFrame(idRef.current);
-  }, deps);
+  }, []);
 
   return idRef;
 }
