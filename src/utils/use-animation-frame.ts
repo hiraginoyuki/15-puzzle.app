432ca73656b9b5fbfee00ceebf41c86ea1a94542
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-05-09 21:32:26 +0900

    Shorten use-animation-frame

--- src/utils/use-animation-frame.ts
+++ src/utils/use-animation-frame.ts
@@ -1,18 +1,16 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { useCallbackRef } from "./use-callback-ref";
 
 export function useAnimationFrame(onFrame: (time: number) => any, deps?: React.DependencyList) {
-  const idRef = useRef(0);
   const callbackRef = useCallbackRef(onFrame, deps || []);
 
   useEffect(() => {
+    let id: number;
     function frame(time: number) {
       callbackRef.current(time);
-      idRef.current = requestAnimationFrame(frame);
+      id = requestAnimationFrame(frame);
     }
-    idRef.current = requestAnimationFrame(frame);
-    return () => cancelAnimationFrame(idRef.current);
+    id = requestAnimationFrame(frame);
+    return () => cancelAnimationFrame(id);
   }, []);
-
-  return idRef;
 }
