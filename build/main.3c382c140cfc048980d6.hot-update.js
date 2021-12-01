1f6ec51cd1b150030293e897dd3eb4aa23d380e7
Yuki Hiragino <yuki@shiina.family>
2021-12-01 15:47:48 +0900

    Updates

---
+++ build/main.3c382c140cfc048980d6.hot-update.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_15_puzzle"]("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[0].use[1]!./node_modules/sass-loader/dist/cjs.js!./src/renderer/renderer.scss":
+/*!*********************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[0].use[1]!./node_modules/sass-loader/dist/cjs.js!./src/renderer/renderer.scss ***!
+  \*********************************************************************************************************************************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__);\n// Imports\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \".fifteenPuzzleRenderer__3H-LT19H {\\n  user-select: none;\\n  -webkit-user-select: none;\\n  width: calc(var(--piece-size) * var(--columns));\\n  height: calc(var(--piece-size) * var(--rows) + 80px);\\n  --piece-size: 80px;\\n  --margin: 0.05;\\n}\\n.fifteenPuzzleRenderer__3H-LT19H > .timerCanvas__3jnsl4pC {\\n  width: 320px;\\n  height: 80px;\\n}\\n.fifteenPuzzleRenderer__3H-LT19H > .piece__352UihZq {\\n  display: flex;\\n  position: absolute;\\n  width: calc(var(--piece-size) * (1 - var(--margin)));\\n  height: calc(var(--piece-size) * (1 - var(--margin)));\\n  margin: calc(var(--piece-size) * var(--margin) / 2);\\n  transform: translate(calc(var(--piece-size) * var(--x)), calc(var(--piece-size) * var(--y)));\\n  border-radius: calc(var(--piece-size) / 16);\\n  background-color: #111;\\n  font-weight: 900;\\n  filter: invert(0%);\\n  transition: 0.1s transform var(--easeOutCirc), 0.2s filter var(--easeOutCirc);\\n  z-index: 0;\\n}\\n.fifteenPuzzleRenderer__3H-LT19H > .piece__352UihZq.hidden__2XUYyEe7 {\\n  opacity: 0;\\n}\\n.fifteenPuzzleRenderer__3H-LT19H > .piece__352UihZq.correct__2lQHd6Li {\\n  filter: invert(100%);\\n}\\n.fifteenPuzzleRenderer__3H-LT19H > .piece__352UihZq > .content__181mr6VW {\\n  margin: auto;\\n  font-size: calc(var(--piece-size) * 9 / 20);\\n}\\n.fifteenPuzzleRenderer__3H-LT19H > .tapListeners__1zzoOVpj {\\n  display: grid;\\n  width: 100%;\\n  height: calc(100% - 80px);\\n  grid-template-columns: repeat(var(--columns), 1fr);\\n}\\n.fifteenPuzzleRenderer__3H-LT19H > .tapListeners__1zzoOVpj > .listener__2ay2qgv9 {\\n  z-index: 1;\\n  background: hsla(calc(360deg * (var(--x) + var(--y) * var(--columns)) / (var(--columns) * var(--rows))), 50%, 50%, 25%);\\n}\", \"\"]);\n// Exports\n___CSS_LOADER_EXPORT___.locals = {\n\t\"fifteenPuzzleRenderer\": \"fifteenPuzzleRenderer__3H-LT19H\",\n\t\"timerCanvas\": \"timerCanvas__3jnsl4pC\",\n\t\"piece\": \"piece__352UihZq\",\n\t\"hidden\": \"hidden__2XUYyEe7\",\n\t\"correct\": \"correct__2lQHd6Li\",\n\t\"content\": \"content__181mr6VW\",\n\t\"tapListeners\": \"tapListeners__1zzoOVpj\",\n\t\"listener\": \"listener__2ay2qgv9\"\n};\n/* harmony default export */ __webpack_exports__[\"default\"] = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://15-puzzle/./src/renderer/renderer.scss?./node_modules/css-loader/dist/cjs.js??ruleSet%5B1%5D.rules%5B0%5D.use%5B1%5D!./node_modules/sass-loader/dist/cjs.js");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "5fade26da25811a04219"; }
+/******/ }();
+/******/ 
+/******/ }
+);
